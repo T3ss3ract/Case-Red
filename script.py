a34d9e6171757f4cb07250b0f4f9bc001bf28a20
= <sayanpsur@gmail.com>
2019-03-09 02:34:10 -0600

    initialized tests

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 from gpiozero import LED, Button
 import yaml
-import io 
+import io
+import time
 
 with open("config.yaml" , 'r') as stream:
 	config = yaml.load(stream)
@@ -8,7 +9,44 @@ with open("config.yaml" , 'r') as stream:
 if config == None:
 	print("ERROR: no config file loaded")
 
+#   check_plant_time
+#   params: none
+#   returns: an integer operation code for the status of each plant 
+#   status code -1: plant is inactive due to time constraints
+#                0: plant is active and receiving nutrients
+#                2: general error code (add more later)
+def check_plant_time():
+    for plant in config[plants]:
+        start_time = config[plant][start_time]
+        end_time = config[plant][end_time]
+        #current_time = time.asmtime
+        if(#current time is past end time):
+            plant_id = config[plant][plant_id]
+    return 0
 
+
+
+
+def fan_test():
+    return 0
+
+def water_pump_test():
+    return 0:
+
+
+def led_test():
+    return 0
+
+
+def run_fans():
+    return 0
+
+
+def run_water_pumps():
+    return 0
+
+def run_leds():
+    return 0
 
 
 
