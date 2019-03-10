9549747cd95945855aa30bcc5fe3fdacee66956e
Caernavron <vayentha@vayentha.xyz>
2019-03-10 07:05:17 +0000

    datetime functionality added part 2

--- timetest.py
+++ timetest.py
@@ -3,6 +3,7 @@ import io
 from datetime import datetime as dt
 
 from datetime import timedelta
+import yaml
 
 print("testing!")
 
@@ -16,8 +17,23 @@ def days_between(d1, d2):
     return abs((d2-d1).days)
 
 
+with open("config.yaml", 'r') as stream:
+    config = yaml.load(stream)
 
-date1 = "2019-8-15"
-date2 = "2019-10-3"
-print(days_between(date1, date2))
+
+
+for key,val in config.items():
+    start_date =  val["start_time"]
+    end_date = val["end_time"]
+    print(start_date)
+    print(end_date)
+    print("calculating time between...")
+    print(days_between(start_date, end_date))
+
+
+
+
+#date1 = "2019-8-15"
+#date2 = "2019-10-3"
+#print(days_between(date1, date2))
     
